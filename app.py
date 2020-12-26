e820b62cd81dd4b880995e3c23a4635b4f0c0296
Balint Kozak <kozak.balint15@gmail.com>
2020-12-26 15:27:50 +0100

    Encoding error fixed and .gitignore added.

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ ITEM_LIST = []
 
 class Item:
     """A movie or a TV show information."""
+
     def __init__(self, imdb_id, title, rating, url):
         self.imdb_id = imdb_id
         self.title = title
@@ -31,13 +32,13 @@ def get_watchlist():
     if path.exists(FILE_PATH):
         remove(FILE_PATH)
     response = requests.get(FILE_URL)
-    open('watchlist.csv', 'wb').write(response.content)
+    open("watchlist.csv", "wb").write(response.content)
     print("Watchlist updated.")
 
 
 def fill_list():
     """Fill a list with films and Tv shows from my IMDb watchlist."""
-    with open('watchlist.csv', 'rt') as data:
+    with open("watchlist.csv", "r", encoding="ISO-8859-1") as data:
         data = csv.reader(data)
         for row in data:
             ITEM_LIST.append(Item(row[0], row[5], row[8], row[6]))
@@ -45,9 +46,9 @@ def fill_list():
 
 def pick_random():
     """Pick a random item (movie or TV show) from the list."""
-    random_id = random.randrange(len(ITEM_LIST)+1)
+    random_id = random.randrange(len(ITEM_LIST) + 1)
     for items in ITEM_LIST:
-        if items.id == str(random_id):
+        if items.imdb_id == str(random_id):
             print(f"Picked item: {items.title}")
             webbrowser.open(items.url)
 
